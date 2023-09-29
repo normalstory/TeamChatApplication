bd0c21362680c32f5eef27dc456d9421d3301877
everpresent@hanmail.net <everpresent@hanmail.net>
2023-09-29 18:11:02 +0900

    기본 디렉토리 구성 및 테스트

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <p className='text-3xl font-bold text-indigo-500'>
+        안녕, Discord Copy냐옹 
+      </p>
+      <Button>
+        이거 Click냥
+      </Button>
+    </div>
+  )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+//단축키 sfc
+const TestPage = () => {
+    return ( 
+        <div> Test Page !</div>
+     );
+}
+ 
+export default TestPage
