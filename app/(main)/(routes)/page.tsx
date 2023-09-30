3216a2cddde91e6a2b4cb4ba96783734ad3071f6
everpresent@hanmail.net <everpresent@hanmail.net>
2023-09-30 12:42:21 +0900

    clerk를 활용해서 로그인/로그아웃 GUI 구성

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,15 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-bold text-indigo-500'>
-        안녕, Discord Copy냐옹 
-      </p>
-      <Button>
-        이거 Click냥
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
--- app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-//단축키 sfc
-const TestPage = () => {
-    return ( 
-        <div> Test Page !</div>
-     );
-}
- 
-export default TestPage
