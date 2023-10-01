ce91aecdaaaeb605d66f17e09240d451d0b95116
ChanWoo <everpresent@hanmail.net>
2023-10-01 12:36:07 +0900

    UploadThing Setup2( FileRoute 설정 및 결과 확인)

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import {X} from "lucide-react";
+import Image from "next/image";
+
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 
@@ -10,15 +13,28 @@ interface FileUploadProps {
   }
 
 export const FileUpload = ({ onChange, value, endpoint}: FileUploadProps) =>{
-    return (
-        <UploadDropzone
-          endpoint={endpoint}
-          onClientUploadComplete={(res) => {
-            onChange(res?.[0].url);
-          }}
-          onUploadError={(error: Error) => {
-            console.log(error);
-          }}
-        />
+  const fileType = value?.split(".").pop();
+  
+  if (value && fileType !=="pdf"){
+    return(
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full"/>
+        <button onClick={()=>onChange("")} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" type="button">
+          <X className="h-4 w-4"/>
+        </button>
+      </div>
     )
+  }
+
+  return (
+      <UploadDropzone
+        endpoint={endpoint}
+        onClientUploadComplete={(res) => {
+          onChange(res?.[0].url);
+        }}
+        onUploadError={(error: Error) => {
+          console.log(error);
+        }}
+      />
+  )
 }
